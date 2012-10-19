1bfdbabd3dab8f49860a5daa8747f34abd1dda56
Zachary Tribbett <tribbettz.dev@gmail.com>
2012-10-19 00:54:13 -0400

    adding List and Detail views for posts and comments; adding MongoDB.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+def register_blueprints(app):
+	from app.views import posts
+	app.register_blueprint(posts)
+
 app = Flask(__name__)
+app.config["MONGODB_DB"] = 'my_tumble_log'
+app.config["SECRET_KEY"] = '\xf0\xa8\x0ee\xc4+\x95}\x7fr\x1cRDAq4\xc8\xe6\x88\xde\x8e&Q\xe8'
+
+db = MongoEngine(app)
+register_blueprints(app)
 
 if __name__ == '__main__':
 	app.run()
